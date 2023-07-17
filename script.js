671d71db22e81b536a2df6affcb228c1226d1caa
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2023-07-17 18:33:44 +0000

    comeco

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+function Comeca(){
+    jogo=document.querySelector(".jogo");
+    for(var c=0;c<20;c++){
+        v.push([]);
+        for(var i=0;i<9;i++){
+            v[c].push("");
+            div=document.createElement("div");
+            div.setAttribute("id",""+c+i);
+            jogo.appendChild(div);
+        }
+    }
+}
+var v=[];
+Comeca();
